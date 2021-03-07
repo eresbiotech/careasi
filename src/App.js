9fe239f299c3d05e09094bc8b60e6514572f43e0
Emre YILMAZ <emreyilmaz@yandex.com>
2021-03-07 05:03:39 +0300

    fixed #2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import banner1 from './images/banner/banner-1.jpg';
 import jQuery from 'jquery';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import ReactGA from 'react-ga';
 let trackingID = "UA-116566614-6";
 let createUUID = () =>{
@@ -323,7 +323,34 @@ const linksHandler = (setQuestions,setLinks,setTitle)=>{
         }
     }
 }
-
+function Header({page,setPage}) {
+    return <header className="header-section">
+        <div className="container">
+            <div className="header-wrapper">
+                <div className="logo-area">
+                    <div className="logo">
+                        <a href="index.html">
+                            <img src="/assets/images/default-monochrome-white.svg" alt="logo"/>
+                        </a>
+                    </div>
+                </div>
+                <ul className="menu">
+                    <li>
+                        <a href="/">Anasayfa</a>
+                    </li>
+                    <li>
+                        <a onClick={()=>{setPage(<AboutUs/>);}} href="#">Hakkımızda</a>
+                    </li>
+                </ul>
+                <div className="header-bar d-lg-none">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    </header>;
+}
 function QuestionsSection() {
     const [questions,setQuestions] = useState(questionWorkFlow);
     const [links,setLinks] = useState([]);
@@ -363,6 +390,98 @@ function QuestionsSection() {
         </>
     );
 };
+function AboutUs() {
+            ReactGA.event({
+                category:"Pageview",
+                action: "Hakkımızda",
+                label: "Hakkımızda"
+            });
+    return <>
+        <BannerSection title="Hakkımızda"/>
+        <div className="billing-section padding-bottom" style={{marginTop: "-83px"}}>
+            <div className="container">
+                <div className="how-search-wrapper">
+                    <p><b>Covid-19</b> mücadelesinde aşının önemini ve aşıyla ilgili bilgi karmaşasını gördüğümüz için <a href="https://www.smallscaled.com" target="_blank"> Burcu Tepekule</a>, <a href="https://emre.xyz" target="_blank">Emre Yılmaz</a> ve <a href="https://elcineksi.com" target="_blank">Elçin Ekşi</a> olarak, bir araya gelip, <a href="https://www.bilimma.com" target="_blank" title="Bilimsel Haber Sitesi">bilimma.com</a>’un desteğiyle <i>Çare Aşı</i> projesini hayata geçirdik.</p>
+                    <p>
+                        Covid-19 mücadelesi küresel olarak sürüyor. Bu mücadelede sağlık çalışanlarının elindeki en büyük mücadele aracı ise kısa sürede ve yüksek maliyetlerle geliştirilen Covid-19 aşıları. Küresel olarak hızla devam eden aşılama çalışmalarının ilerlemesi önünde çeşitli engeller bulunuyor. Bunlardan birisi insanları yanlış bilgilendiren ve komplo teorileriyle insanları yanlış yönlendirmekten hoşlananların yaptığı kampanyalar.</p>
+                    <p>Bu yanlış bilgilendirme kampanyalarıyla mücadeleye yardımcı olması için bir araya gelerek çeşitli kaynaklara kolay erişim sağlayacak bir platform geliştirdik. Amacımız insanların aklına takılan sorulara kolay ve hızlı yanıt bulmasını sağlamak oldu. Böylece yanlış bilgi hızlı yayılsa da biz de doğru bilginin kolay erişilebilir olmasını istedik.</p>
+                    <p>Çevrenizde farklı yaş gruplarında ve çeşitli topluluklarda bu siteyi paylaşarak daha fazla kişiye ulaştırabilir bu çalışmalarda siz de bize katılabilirsiniz. Sitedeki içerikler ve bağlantılarla ilgili tavsiye, iyileştirme ve diğer önerileriniz için bize <a href="mailto:bilgi@careasi.org">bilgi@careasi.org</a> adresine e-posta göndererek ulaşabilirsiniz.</p>
+                    <br/>
+                    <p>
+                        Bizimle olduğunuz için teşekkürler.
+                    </p>
+                </div>
+
+                <div className="how-search-wrapper" style={{marginTop:"20px"}}>
+                    <h4>Burcu Tepekule Kimdir?</h4>
+                    <br/>
+                    <p>Antibiyotik direnci evrimi, epidemiyoloji, mikrobiyom modellemesi, zaman serileri analizi ve sinyal işleme ve son dönemde Covid-19 epidemiyolojik modelleme alanında çalışmalar yapan Zürih Üniversite Hastanesi’nde doktora sonra araştırmacı.</p>
+                    <ul className="social-icons" style={{backgroundColor:"#372c7a",borderRadius:"6px"}}>
+                        <li>
+                            <a href="https://www.smallscaled.com" title="Burcu Tepekule kişisel web sayfası" target="_blank"><i className="fas fa-globe-asia"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/burcutepekule/" title="Burcu Tepekule linkedin hesabı" target="_blank"><i className="fab fa-linkedin"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/burcutepekule" target="_blank" title="Burcu Tepekule twitter hesabı"><i className="fab fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://scholar.google.com/citations?user=mNuCT7MAAAAJ&hl=en" title="Burcu Tepekule Schoolar profili"><i className="fas fa-graduation-cap"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/burcutepekule" target="_blank" title="Burcu Tepekule github adresi"><i className="fab fa-github"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="how-search-wrapper" style={{marginTop:"20px"}}>
+                    <h4>Elçin Ekşi Kimdir?</h4>
+                    <br/>
+                    <p>Ekoloji ve evrimle ilgilenen biyolog. Orta Doğu Teknik Üniversitesi’nde arkeometri alanında yüksek lisansına devam ediyor. Anadolu’nun holosen dönemi faunasını inceliyor. <a href="https://www.eresbiotech.com" target="_blank"> ERES Biyoteknoloji</a>’de Emre ile birlikte projelerine devam ediyor.  <a href="https://bilimkutusu.com" target="_blank" title="Bilimsel Aktivite Kutusu">Bilim Kutusu</a> ve <a href="https://bilimma.com" title="Bilimsel haber platformu" target="_blank">Bilimma.com</a> projelerinde ise kurucu, içerik ve ürün geliştirici.</p>
+                    <ul className="social-icons" style={{backgroundColor:"#372c7a",borderRadius:"6px"}}>
+                        <li>
+                            <a href="https://elcineksi.com" title="Elçin Ekşi kişisel web sayfası" target="_blank"><i className="fas fa-globe-asia"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/el%C3%A7in-ek%C5%9Fi/" title="Elçin Ekşi linkedin hesabı" target="_blank"><i className="fab fa-linkedin"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/amaeksi" target="_blank" title="Elçin Ekşi twitter hesabı"><i className="fab fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://scholar.google.com.tr/citations?user=WNhtRggAAAAJ&hl=tr" title="Elçin Ekşi Schoolar profili"><i className="fas fa-graduation-cap"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/amaeksi" target="_blank" title="Elçin Ekşi github adresi"><i className="fab fa-github"></i></a>
+                        </li>
+                    </ul>
+                </div>
+
+                                <div className="how-search-wrapper" style={{marginTop:"20px"}}>
+                    <h4>Emre Yılmaz Kimdir?</h4>
+                    <br/>
+                                    <p>Web uygulamaları geliştiricisi. <a href="https://bilimkutusu.com" target="_blank" title="Bilimsel Aktivite Kutusu">Bilim Kutusu</a> ve <a href="https://bilimma.com" title="Bilimsel haber platformu" target="_blank">Bilimma.com</a> projelerinde kurucu, <a href="https://www.eresbiotech.com" target="_blank" title="Eres Biyoteknoloji web sayfası">ERES Biyoteknoloji</a>’de ortak.</p>
+                    <ul className="social-icons" style={{backgroundColor:"#372c7a",borderRadius:"6px"}}>
+                        <li>
+                            <a href="https://emre.xyz" title="Emre Yılmaz kişisel web sayfası" target="_blank"><i className="fas fa-globe-asia"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/delirehberi/" title="Emre Yılmaz linkedin hesabı" target="_blank"><i className="fab fa-linkedin"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/delirehberi" target="_blank" title="Emre Yılmaz twitter hesabı"><i className="fab fa-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/delirehberi" target="_blank" title="Emre Yılmaz github adresi"><i className="fab fa-github"></i></a>
+                        </li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+    </>;
+}
 function LinksSection({links}) {
 
     return     <div className="billing-section padding-bottom" style={{marginTop: "-83px"}}>
@@ -396,7 +515,7 @@ function LinksSection({links}) {
 
 function BannerSection({title}) {
     return (
-        <section className="banner-1 bg_img" style={{backgroundImage: "url('/assets/images/banner/banner-1.jpg')"}}>
+        <section className="banner-1 bg_img" style={{backgroundImage: "url("+banner1+")"}}>
             <div className="container">
                 <div className="banner-content cl-white">
                     <h1 className="title">{title}</h1>
@@ -409,9 +528,18 @@ function BannerSection({title}) {
 
 function App() {
     ViewEf(jQuery);
+    let [page,setPage] = useState(<></>);
+    useEffect(()=>{
+        setPage(<QuestionsSection/>);
+    },[]);
+    useEffect(()=>{
+            window.location = "#root";
+    },[page])
+
     return (
         <div>
-            <QuestionsSection/>
+            <Header setPage={setPage} page={page}/>
+            {page}
         </div>
     );
 }
